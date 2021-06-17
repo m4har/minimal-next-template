4089eb91c1f58dd99a4ca214ef3bd25684d529ff
mahardicka nurachman <mahardicka404@gmail.com>
2021-06-17 13:52:30 +0700

    create seo component

---
+++ components/seo.tsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+
+interface Props {
+  title?: string;
+  desc?: string;
+  icon?: string;
+}
+function Seo(props: Props) {
+  const { title, desc, icon } = props;
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={desc} />
+      <link rel="icon" href={icon} />
+    </Head>
+  );
+}
+
+Seo.defaultProps = {
+  title: "",
+  desc: "",
+  icon: "/favicon.ico",
+};
+
+export default Seo;
