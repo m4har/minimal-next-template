10cad2ee37686277b63a77252cf6972f97cd64ef
mahardicka nurachman <mahardicka404@gmail.com>
2021-06-17 14:11:51 +0700

    protected pages

---
+++ pages/home.tsx
@@ -0,0 +1,20 @@
+import protectHOC from "@hoc/proteced-route";
+import useAuth from "@stores/auth-persist";
+import Button from "@components/button";
+import Seo from "@components/seo";
+
+function Home() {
+  const { onLogout } = useAuth((state) => state);
+
+  return (
+    <div>
+      <Seo title="Home" />
+      <main className="h-screen flex items-center justify-center flex-col">
+        <h1>HOME</h1>
+        <Button label="LOGOUT" onClick={onLogout} />
+      </main>
+    </div>
+  );
+}
+
+export default protectHOC(Home);
