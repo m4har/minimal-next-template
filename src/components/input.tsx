5948df7f831b7a31852ada65cd206fa1ac025eba
mahardicka nurachman <mahardicka404@gmail.com>
2021-06-19 12:44:26 +0700

    input support form hooks

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,21 +1,22 @@
-import { InputHTMLAttributes, memo } from "react";
+import { InputHTMLAttributes, memo, forwardRef, LegacyRef } from "react";
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   placeholder: string;
 }
-function Input(props: Props) {
+const Input = forwardRef<HTMLInputElement, Props>((props, ref) => {
   const { label, placeholder } = props;
   return (
     <div className="my-4">
       <p>{label}</p>
       <input
         {...props}
+        ref={ref}
         className="border-2 rounded px-2 h-10 w-48"
         placeholder={placeholder}
       />
     </div>
   );
-}
+});
 
 export default memo(Input);
